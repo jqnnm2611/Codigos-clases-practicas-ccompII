25459c27f77b7dbe3c32b171c31384033f7d5638
Joaquín Núñez <joaquin.nunez@ucsp.edu.pe>
2026-03-15 12:09:34 -0500

    Create Practica 1: Ejercicio 1.cpp

---
+++ Practica 1: Ejercicio 1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //ALINEAR LOS CARACTERES DE UN ARRAY A LA DERECHA
+
+    //solucion con indices
+    int tam = 11;
+    char A[11] = {'c','+','+',' ','j','a','v','a',' ',' ',' '};
+    int i = tam - 1;
+    int espacios = 0;
+    while(A[i] == ' '){
+        espacios++;
+        i--;
+    }
+    
+    for(int i = 0; i < tam; i++)
+        cout << A[i] << ",";
+    cout << endl;
+    
+    for(int i = tam - 1 - espacios; i >= 0; i--){
+        A[i + espacios] = A[i]; 
+        A[i] = ' ';
+    }
+    
+    cout << espacios << endl;
+    
+    for(int i = 0; i < tam; i++)
+        cout << A[i] << ",";
+    cout << endl;
+
+    
+    //solucion con punteros (revisada por profe)
+    int tam = 11;
+    char A[11] = {'c','+','+',' ','j','a','v','a',' ',' ',' '};
+    char* i = A + tam - 1;
+    int espacios = 0;
+    while(*i == ' '){
+        espacios++;
+        i--;
+    }
+    
+    for(char* p = A; p < A + tam; p++)
+        cout << *p << ",";
+    cout << endl;
+    
+    for(char* p = A + tam - 1 - espacios; p >= A; p--){
+        *(p + espacios) = *p; 
+        *p = ' ';
+    }
+    
+    cout << espacios << endl;
+    
+    for(char* p = A; p < A + tam; p++)
+        cout << *p << ",";
+    cout << endl;
+
+    return 0;
+}
