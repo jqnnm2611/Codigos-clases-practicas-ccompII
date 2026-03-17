3a44528ae59c69a635cbbaa765026f5222748729
Joaquín Núñez <joaquin.nunez@ucsp.edu.pe>
2026-03-16 21:44:00 -0500

    Create Práctica 1: Ejercicio 2 (segunda version).cpp

---
+++ Práctica 1: Ejercicio 2 (segunda version).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//REDUCCIÓN DE ARRAY
+
+int main()
+{
+    int n = 11;
+    int A[n] = {3,1,0,-5,2,3,4,2,0,1,5};
+    
+    for(int* p = A; p < A + n; p++)
+        cout << *p << ",";
+    cout << endl;
+    
+    int m = 3;
+    bool reduccion = true;
+    
+    while(reduccion){
+        for(int* p = A; p < A + n; p = p + m){
+            int suma = 0;
+            for(int* q = p; q < p + m; q++){
+                if(q >= A + n || q+1 >= A + n || q+2 >= A + n){
+                    suma = 0;
+                    break;
+                }
+                suma = suma + *q;
+            }
+            *p = suma;
+            *(p+1) = -1;
+            *(p+2) = -1;
+        }
+        for(int* p = A; p < A + n; p++)
+            cout << *p << ",";
+        cout << endl;
+        return 0;
+    }
+
+    return 0;
+}
