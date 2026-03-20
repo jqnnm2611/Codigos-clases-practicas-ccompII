89588dfefdd2f9651bed25e8d87aeb8fe6047417
Joaquín Núñez <joaquin.nunez@ucsp.edu.pe>
2026-03-20 08:29:53 -0500

    Create Ejercicios en clase: Ejercicio 2.cpp

---
+++ Ejercicios en clase: Ejercicio 2.cpp
@@ -0,0 +1,35 @@
+//ELIMINAR NUMEROS CONSECUTIVOS IGUALES HASTA QUE YA NO QUEDEN MÁS ENTEROS CONSECUTIVOS IGUALES. COLOCAR 0 EN LAS POSICIONES RESTANTES
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n = 7;
+    int A[n] = {1,3,2,2,1,1,3};
+    
+    //imprimir
+    for(int*a = A; a < A+n; a++)
+        cout << *a << ',';
+    cout << endl;
+    
+    
+    //algoritmo
+    bool rdc = true;
+    while(rdc){
+        for(int*p = A; p < A + n; p++){
+            for(int*q = p+1; *q == *p; q++){
+                cout << "prueba" << endl;
+            }
+        }
+        rdc = false;
+    }
+    
+    //imprimir
+    for(int*a = A; a < A+n; a++)
+        cout << *a << ',';
+    cout << endl;
+
+
+    return 0;
+}
