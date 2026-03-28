11da748b9a19e624a3cc425fe267441ccbdb3fe5
Joaquín Núñez <joaquin.nunez@ucsp.edu.pe>
2026-03-28 08:24:32 -0500

    Create Práctica 3: Ejercicio 4.cpp

---
+++ Práctica 3: Ejercicio 4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int tam = 12;
+    int A[tam] = {1,3,2,4,0,0,0,0,0,0,0,0};
+    int n = 3;
+    
+    for(int* p = A; p < A + tam; p++)
+        cout << *p << ',';
+    cout << endl;
+    
+    int* a = A + tam - 1;
+    while(*a == 0)
+        a--;
+    
+    for(int* b = A + tam - 1; b >= A; b = b - n, a--){
+        for(int* c = b; c > b - n; c--)
+            *c = *a;
+    }
+    
+    for(int* p = A; p < A + tam; p++)
+        cout << *p << ',';
+    cout << endl;
+
+    return 0;
+}
