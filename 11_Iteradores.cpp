bea952c0e63d65be696d8cfbeb99a89217d6cb8f
Joaquín Núñez <joaquin.nunez@ucsp.edu.pe>
2026-05-29 08:00:47 -0500

    Create 11_Iteradores.cpp

---
+++ 11_Iteradores.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool mult(int x, int y){
+    if(x % y == 0)
+        return true;
+    return false;
+}
+
+vector<int> find_matching(vector<int> v, bool(*f)(int, int)){
+    vector<int> res;
+    vector<int>::iterator it = v.begin();
+    vector<int>::iterator end = v.end();
+    while(it != end){
+        if(f(*it, 2))
+            res.push_back(*it);
+        it++;
+    }
+    return res;
+}
+
+int main() {
+    
+    vector<int> v1;
+    v1.push_back(1);
+    v1.push_back(2);
+    v1.push_back(3);
+    v1.push_back(4);
+    v1.push_back(5);
+    
+    bool(*f1)(int, int) = mult;
+    vector<int> v2 = find_matching(v1, f1);
+    vector<int>::iterator it = v2.begin();
+    vector<int>::iterator end = v2.end();
+    while(it != end){
+      cout << *it << ',';
+      it++;
+      }
+    cout << endl;
+    
+    return 0;
+}
